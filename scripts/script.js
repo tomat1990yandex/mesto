b1bd0294ebf29ab02f6138442e3c4adc6eabefc9
Dmitriy Orlov <tomat1990@yandex.ru>
2021-05-28 17:53:15 +0500

    hotfix: adaptive and other 2

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,10 @@
 let popup = document.querySelector('.popup');
 
 // кнопка вызова окна редактирования профиля
-let ButtonPopup = document.querySelector('.profile__edit');
+let buttonPopup = document.querySelector('.profile__edit');
 
 // кнопка закрытие окна редактирования профиля
-let ButtonClose = document.querySelector('.popup__close');
+let buttonClose = document.querySelector('.popup__close');
 
 // поле ввода имени
 let profileName = document.querySelector('.profile__name');
@@ -26,25 +26,25 @@ let form = document.querySelector('.popup__form');
 // открытие окна popup для редактирования профиля
 // перенос имени и профессии с основной страницы в popup
 function open() {
-  popupName.value = profileName.textContent
-  popupProfession.value = profileProfession.textContent
-  popup.classList.add('popup_opened')
+  popupName.value = profileName.textContent;
+  popupProfession.value = profileProfession.textContent;
+  popup.classList.add('popup_opened');
 }
 
 // закрытие окна popup для редактирования профиля
 function close() {
-  popup.classList.remove('popup_opened')
+  popup.classList.remove('popup_opened');
 }
 
 // перенос имени и профессии с popup в основную страницу
 function formSubmitHandler(save) {
-  save.preventDefault()
-  profileName.textContent = popupName.value
-  profileProfession.textContent = popupProfession.value
-  close()
+  save.preventDefault();
+  profileName.textContent = popupName.value;
+  profileProfession.textContent = popupProfession.value;
+  close();
 }
 
 // события при действиях
-ButtonPopup.addEventListener('click', open);
-ButtonClose.addEventListener('click', close);
+buttonPopup.addEventListener('click', open);
+buttonClose.addEventListener('click', close);
 form.addEventListener('submit', formSubmitHandler);
