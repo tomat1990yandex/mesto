c79a6249806678d48a26e19465d5b7aa58c4d816
Dmitriy Orlov <tomat1990@yandex.ru>
2021-06-06 20:19:52 +0500

    project 5: add delete button

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 // окно popup
 let popup = document.querySelector('.popup');
 
@@ -22,13 +49,58 @@ let popupProfession = document.querySelector('#popupProfession');
 // блок формы
 let form = document.querySelector('.popup__form');
 
+//
+let popupTitle = document.querySelector('.popup__title');
 
-// открытие окна popup для редактирования профиля
+// элемент шаблона
+const itemTemplate = document.querySelector('.element__template').content;
+
+// элемент списка
+const list = document.querySelector('.elements');
+
+// применение массива для каждого значения функции
+function renderItems() {
+  initialCards.forEach(renderItem);
+}
+
+// функция создания карточек по шаблону с использованием значений массива
+function renderItem(element) {
+  const htmlElement = itemTemplate.cloneNode(true);
+  htmlElement.querySelector('.element__title').textContent = element.name;
+  htmlElement.querySelector('.element__image').src = element.link;
+  setEventListeners(htmlElement);
+
+  list.appendChild(htmlElement);
+}
+renderItems();
+
+// удаление карточки
+function handleDelete(evt) {
+  evt.target.closest('.element').remove();
+}
+
+function setEventListeners(element1) {
+  element1.querySelector('.element__delete').addEventListener('click', handleDelete);
+
+}
+
+// открытие окна popup
+function openPopup() {
+  popup.classList.add('popup_opened');
+}
+
 // перенос имени и профессии с основной страницы в popup
-function open() {
+function fillinProfile() {
   popupName.value = profileName.textContent;
   popupProfession.value = profileProfession.textContent;
-  popup.classList.add('popup_opened');
+}
+
+// Открытие popup через кнопку редактирования профиля
+
+function editProfileBtn() {
+  openPopup();
+  popupTitle.querySelector('.popup__title').textContent = 'Редактировать профиль';
+
 }
 
 // закрытие окна popup для редактирования профиля
@@ -45,6 +117,6 @@ function formSubmitHandler(save) {
 }
 
 // события при действиях
-buttonPopup.addEventListener('click', open);
+buttonPopup.addEventListener('click', editProfileBtn);
 buttonClose.addEventListener('click', close);
 form.addEventListener('submit', formSubmitHandler);
