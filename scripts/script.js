266721eaf8e8d0524da7bf5e05145857a7082796
Dmitriy Orlov <tomat1990@yandex.ru>
2021-06-09 12:39:02 +0500

    project 5: prepare to review

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,28 @@
+// стартовый массив из 6 карточек
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: 'Челябинск',
+    link: 'https://images.unsplash.com/photo-1617386396687-fba8361df419?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: 'Екатеринбург',
+    link: 'https://images.unsplash.com/photo-1591389662006-e4ffc8c9708e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
   },
   {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: 'Омск',
+    link: 'https://images.unsplash.com/photo-1558708103-b5f79ddcd646?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80'
   },
   {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: 'Владивосток',
+    link: 'https://images.unsplash.com/photo-1587637885131-8b08567433d6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80'
   },
   {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: 'Казань',
+    link: 'https://images.unsplash.com/photo-1561398024-910a2c6cf1a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
   },
   {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    name: 'Иркутск',
+    link: 'https://images.unsplash.com/photo-1577094337070-c2ad44c1c9b4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
   }
 ];
 
@@ -31,17 +32,11 @@ const popupEditBtn = document.querySelector('#popupEdit');
 // окно popup добавления карточки
 const popupAddBtn = document.querySelector('#popupAdd');
 
-// окно popup увеличения карточки
-const popupImgBtn = document.querySelector('#popupImg');
-
 // кнопка вызова окна редактирования профиля
 const buttonPopupEdit = document.querySelector('.profile__edit');
 
 // кнопка вызова окна добавления карточки
 const buttonPopupAdd = document.querySelector('.profile__add');
-
-// изображение карточки
-let buttonImg = document.querySelector('.element__image');
 
 // кнопка закрытие окна редактирования профиля
 const buttonCloseEdit = document.querySelector('#ButtonCloseEdit');
@@ -49,7 +44,7 @@ const buttonCloseEdit = document.querySelector('#ButtonCloseEdit');
 // кнопка закрытие окна добавления карточки
 const buttonCloseAdd = document.querySelector('#ButtonCloseAdd');
 
-// поле ввода имени
+// блок имени профиля
 const profileName = document.querySelector('.profile__name');
 
 // поле ввода профессии
@@ -57,52 +52,48 @@ const profileProfession = document.querySelector('.profile__profession');
 
 // поле ввода имени
 const popupName = document.querySelector('#popupName');
-const popupImage = document.querySelector('#popupImg'); // POPUP
-const imagePopupImage = popupImage.querySelector('#popupImgLink'); // IMAGE
-const buttonClosePopupImage = popupImage.querySelector('#ButtonCloseImg'); // BUTTON close pop-up
-const captionPopupImage = popupImage.querySelector('#popupImgTitle'); //CAPTION
 
-// перенос имени и профессии с popup в основную страницу
-// сброс значений в полях ввода окна добавления карточки
-// закрытие окна popup для редактирования профиля
-// закрытие окна popup для редактирования профиля
-// перенос имени и профессии с основной страницы в popup
-// открытие окна popup через нажатие на карточку
-// открытие окна popup через кнопку добавления карточки
-// открытие окна popup через кнопку редактирования профиля
-// кнопка лайк
-// удаление карточки
-// создание новой карточки через кнопку создать
-// функция создания карточек по шаблону с использованием значений массива или создания новых карточек
-// применение массива для каждого значения функции
-// элемент списка
-// элемент шаблона
-// кнопка отправки формы добавления
+// окно popup увеличения карточки
+const popupImage = document.querySelector('#popupImg');
+
 // поле ввода ссылки на картинку
-// поле ввода названия картинки
-// блок формы
-// поле ввода профессии
+const popupImgLink = popupImage.querySelector('#popupImgLink');
 
+// кнопка закрытие окна увеличенной картинки
+const buttonClosePopupImage = popupImage.querySelector('#ButtonCloseImg');
+
+// поле ввода названия картинки
+const popupImageTitle = popupImage.querySelector('#popupImgTitle');
+
+// поле ввода профессии
 const popupProfession = document.querySelector('#popupProfession');
 
+// блок формы
 const formEdit = document.querySelector('#popupFormEdit');
 
+// поле ввода названия карточки
 const popupNameAdd = document.querySelector('#popupNameAdd');
 
+// поле ввода ссылки на картинку
 const popupLinkAdd = document.querySelector('#popupLinkAdd');
 
+// кнопка создания карточки
 const popupSaveAdd = document.querySelector('#popupSaveAdd');
 
+// блок шаблона
 const itemTemplate = document.querySelector('.element__template').content;
 
+// список для вставки шаблона
 const list = document.querySelector('.elements');
+
+
+// применение массива для каждого значения функции
 function renderItems() {
   initialCards.forEach(renderItem);
 
 }
 
-
-
+// функция создания карточек по шаблону с использованием значений массива или создания новых карточек
 function renderItem(element) {
   let elementTextContent;
   let imageSrc;
@@ -115,6 +106,7 @@ function renderItem(element) {
   }
   const elementTmplt = itemTemplate.cloneNode(true);
   elementTmplt.querySelector('.element__title').textContent = elementTextContent;
+  elementTmplt.querySelector('.element__image').alt = elementTextContent;
 
   elementTmplt.querySelector('.element__image').src = imageSrc;
 
@@ -124,67 +116,88 @@ function renderItem(element) {
 }
 
 renderItems();
+
+// создание новой карточки через кнопку создать
 function handleSubmit(add) {
   add.preventDefault();
   renderItem();
   closeAdd();
 
 }
+
+// удаление карточки
 function handleDelete(evt) {
   evt.target.closest('.element').remove();
 
 }
+
+// слушатели событий карточек
 function setEventListeners(element) {
   element.querySelector('.element__delete').addEventListener('click', handleDelete);
   element.querySelector('.element__like').addEventListener('click', handleLike);
   element.querySelector('.element__image').addEventListener('click', handleImage);
 
 }
+
+// кнопка лайк
 function handleLike(like) {
   like.target.classList.toggle('element__like_active');
 
 }
+
+// открытие окна popup через кнопку редактирования профиля
 function openPopupEdit() {
   fillinProfile();
   popupEditBtn.classList.add('popup_opened');
 
 }
+
+// открытие окна popup через кнопку добавления карточки
 function openPopupAdd() {
   resetAddForm();
   popupAddBtn.classList.add('popup_opened');
 
 }
+
+// открытие окна popup через нажатие на карточку
 function openPopupImg() {
   popupImage.classList.add('popup_opened');
 
 }
+
+// перенос имени и профессии с основной страницы в popup
 function fillinProfile() {
   popupName.value = profileName.textContent;
   popupProfession.value = profileProfession.textContent;
 
 }
 
+// закрытие окна popup для редактирования профиля
 function closeEdit() {
   popupEditBtn.classList.remove('popup_opened');
 
 }
 
+// закрытие окна popup для добавления карточки
 function closeAdd() {
   popupAddBtn.classList.remove('popup_opened');
 
 }
 
+// закрытие окна popup увеличения картинки
 function closeImg() {
   popupImage.classList.remove('popup_opened');
 
 }
 
-
+// сброс значений в полях ввода окна добавления карточки
 function resetAddForm() {
   popupNameAdd.value = null;
   popupLinkAdd.value = null;
 
 }
+
+// перенос имени и профессии с popup в основную страницу
 function formSubmitHandler(save) {
   save.preventDefault();
   profileName.textContent = popupName.value;
@@ -192,22 +205,24 @@ function formSubmitHandler(save) {
   closeEdit();
 
 }
+
+// функция увеличения картинки
 function handleImage(evt) {
-  imagePopupImage.src  = evt.target.closest('.element__image').src;
-  console.log(imagePopupImage.src);
-  imagePopupImage.alt = evt.target.closest('.element__image').alt;
-  captionPopupImage.textContent = evt.target.closest('.element').querySelector('.element__title').textContent;
+  popupImgLink.src = evt.target.closest('.element__image').src;
+  // console.log(imagePopupImage.src);
+  popupImgLink.alt = evt.target.closest('.element__image').alt;
+  popupImageTitle.textContent = evt.target.closest('.element').querySelector('.element__title').textContent;
   openPopupImg(popupImage);
 
 }
 
-buttonClosePopupImage.addEventListener('click', ()=> closeImg(popupImage));
+// закрытие увеличенного изображения с кнопки
+buttonClosePopupImage.addEventListener('click', () => closeImg(popupImage));
 
 
 // события при действиях
 buttonPopupEdit.addEventListener('click', openPopupEdit);
 buttonPopupAdd.addEventListener('click', openPopupAdd);
-// buttonClosePopupImage.addEventListener('click', openPopupImg);
 buttonCloseEdit.addEventListener('click', closeEdit);
 buttonCloseAdd.addEventListener('click', closeAdd);
 formEdit.addEventListener('submit', formSubmitHandler);
