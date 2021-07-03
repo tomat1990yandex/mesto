e5335ba151019668a018ec9c1657767913cfc43e
Dmitriy Orlov <tomat1990@yandex.ru>
2021-07-03 11:51:48 +0500

    project 7: hotfix the template string

--- scripts/Card.js
+++ scripts/Card.js
@@ -21,7 +21,7 @@ class Card {
     this._element = this._getTemplate();
     this._setEventListeners();
 
-    this._element.querySelector('.element__image').src = `${this._link}`;
+    this._element.querySelector('.element__image').src = this._link;
     this._element.querySelector('.element__image').alt = this._name;
     this._element.querySelector('.element__title').textContent = this._name;
 
